45d8391ae6aff2160e1a8230f4f2ee7e43814335
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-10-28 12:14:37 -0300

    Final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var numero1 = parseInt(prompt("Insira um número"));
 
 var numero2 = parseInt(prompt("Insira outro número número"));
 
-var operador = prompt("insira + para somar, - para subtrair, / para dividir ou * para dividir")
+var operador = prompt("Escolha qual operação realizar: insira + para somar, - para subtrair, / para dividir ou * para dividir")
 
 if(operador == "+"){
     resultado =numero1+numero2;
@@ -17,9 +17,11 @@ if(operador == "/"){
     resultado =numero1/numero2;
 } 
 
-document.write(numero1 "<br>")
-document.write(numero2)
-document.write(operador)
+document.write(numero1 + "<br>")
+document.write(operador + "<br>")
+document.write(numero2 + "<br>")
+document.write("=<br>")
+document.write(numero2 + "<br>")
 
 var div_local = document.getElementById("exibe_resultado");
 
