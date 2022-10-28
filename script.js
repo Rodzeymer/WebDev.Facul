5eaa34e921bbd660580c144a3cce91b747c1c302
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2022-10-28 12:19:29 -0300

    com uma função resumindo os 4 if

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ document.write(numero2 + "<br>")
 var div_local = document.getElementById("exibe_resultado");
 
 div_local.innerHTML += resultado;
+
+/* Ou simplificando
+
+var numero1 = parseInt(prompt("Insira um número"));
+var numero2 = parseInt(prompt("Insira outro número número"));
+var operador = prompt("Escolha qual operação realizar: insira + para somar, - para subtrair, / para dividir ou * para dividir")
+
+var resultadoOperacao = realizaOperacao(numero1, numero2, operador);
+
+function realizaOperacao(numero1, numero2, operador){
+    var resultado =0;
+    resultado = eval(numero1 + operador + numero2);
+
+    return resultado;
+}
+
+var div_local = document.getElementById("exibe_resultado");
+div_local.innerHTML += resultadoOperacao;
+
+*/
